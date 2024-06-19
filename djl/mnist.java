5fc0c14a540ce21320b30ee4627efdee685c2355
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-06-19 22:32:25 +0900

    djl mnist

--- djl/mnist.java
+++ djl/mnist.java
@@ -1,16 +1,17 @@
-// camel-k: language=java
+// camel-k: language=java dependency=ai.djl:model-zoo:0.28.0 dependency=ai.djl.pytorch:pytorch-engine:0.28.0 dependency=ai.djl.pytorch:pytorch-jni:2.3.0-0.28.0 dependency=ai.djl.pytorch:pytorch-native-cpu:2.3.0
 
 import org.apache.camel.builder.RouteBuilder;
+import java.io.File;
+import java.util.*;
 
 public class mnist extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
-        // Write your routes here, for example:
-        from("timer:java?period={{time:1000}}")
-            .setBody()
-                .simple("Hello Camel from ${routeId}")
-            .log("${body}");
+        var num = new Random(System.currentTimeMillis()).nextInt(10);
+        fromF("file:data/mnist/%s?noop=true&recursive=true&include=.*\\.png", num)
+            .convertBodyTo(File.class)
+            .to("djl:cv/image_classification?artifactId=mlp")
+            .log("${headers.camelFileName} => ${body.best.className}");
     }
 }
